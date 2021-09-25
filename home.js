c4539cdbdd11336a925f653553670d5c00a9d8e0
l3r0ux <lerouxvermeulen@gmail.com>
2021-09-25 07:31:31 +0200

    add if statement in case 'home.html' is not there when go on site for the first time

--- home.js
+++ home.js
@@ -4,8 +4,16 @@ const fromUrl = window.location.href;
 
 for (let service of services) {
     service.addEventListener('click', (event) => {
-        let replaceFromIndex = window.location.href.match('home').index;
+        let replaceFromIndex;
+        let matchedString = window.location.href.match('home');
+
+        if (matchedString) {
+            replaceFromIndex = matchedString.index;
+        } else {
+            replaceFromIndex = fromUrl.length - 1;
+        }
+
         let toUrl = fromUrl.slice(0, replaceFromIndex);
-        window.location.href = `${toUrl}services-page/services.html?scrollTarget=${service.id}`;
+        window.location.href = `${toUrl}services-page/services.html?scrollTarget=${service.id}`;    
     }) 
 }
